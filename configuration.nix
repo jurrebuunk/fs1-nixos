0d099fe8706c75b21506994e88c10bb087436ce6
jurrebuunk <jurrebuunk@outlook.com>
2025-12-18 11:07:39 +0100

    Configure NixOS as a QEMU guest, add Dutch locale settings, and disable systemd OOMD.

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,23 @@
   # Enable flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nl_NL.UTF-8";
+    LC_IDENTIFICATION = "nl_NL.UTF-8";
+    LC_MEASUREMENT = "nl_NL.UTF-8";
+    LC_MONETARY = "nl_NL.UTF-8";
+    LC_NAME = "nl_NL.UTF-8";
+    LC_NUMERIC = "nl_NL.UTF-8";
+    LC_PAPER = "nl_NL.UTF-8";
+    LC_TELEPHONE = "nl_NL.UTF-8";
+    LC_TIME = "nl_NL.UTF-8";
+  };
+
+  # Disable OOMD if it's failing (common on some kernels/VMs)
+  systemd.oomd.enable = false;
+
   # Basic system configuration
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda"; # Change this to your actual boot device
